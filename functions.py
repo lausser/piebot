1be2db8c2c9145867fc861e872dffaed42bd68ab
Gerhard Lausser <gerhard.lausser@consol.de>
2022-07-07 15:26:24 +0200

    version 1.0.12

--- functions.py
+++ functions.py
@@ -186,8 +186,10 @@ def pre_flight_checks():
 
     # Checks whether the API key and API secret have been defined
     if "API_KEY" in os.environ:
+        print("read API_KEY from environment: {}".format(os.environ["API_KEY"]))
         api_key = os.environ["API_KEY"]
     if "API_SECRET" in os.environ:
+        print("read API_SECRET from environment")
         api_secret = os.environ["API_SECRET"]
     try:
         api_key and api_secret
